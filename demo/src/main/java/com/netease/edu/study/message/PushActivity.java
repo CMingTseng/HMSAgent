a544771bb19be8418a78a055b228550dec46787c
Pengfei Han <hanpfei@outlook.com>
2018-07-17 16:21:59 +0800

    Update the demo.
    Signed-off-by: Pengfei Han <hanpfei@outlook.com>

---
+++ demo/src/main/java/com/netease/edu/study/message/PushActivity.java
@@ -0,0 +1,201 @@
+package com.netease.edu.study.message;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.huawei.android.hms.agent.HMSAgent;
+import com.huawei.android.hms.agent.common.BaseAgentActivity;
+import com.huawei.android.hms.agent.push.handler.DeleteTokenHandler;
+import com.huawei.android.hms.agent.push.handler.EnableReceiveNormalMsgHandler;
+import com.huawei.android.hms.agent.push.handler.EnableReceiveNotifyMsgHandler;
+import com.huawei.android.hms.agent.push.handler.GetPushStateHandler;
+import com.huawei.android.hms.agent.push.handler.GetTokenHandler;
+import com.huawei.android.hms.agent.push.handler.QueryAgreementHandler;
+
+/**
+ * Created by hanpfei0306 on 18-7-17.
+ */
+
+public class PushActivity extends BaseAgentActivity implements PushMessageRevicer.IPushCallback,
+        View.OnClickListener {
+    private static final String TAG = PushActivity.class.getSimpleName();
+
+    private String token;
+    private TextView mTvLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push);
+
+        findViewById(R.id.btn_gettoken).setOnClickListener(this);
+        findViewById(R.id.btn_deletetoken).setOnClickListener(this);
+        findViewById(R.id.btn_getpushstatus).setOnClickListener(this);
+        findViewById(R.id.btn_setnormal).setOnClickListener(this);
+        findViewById(R.id.btn_setnofify).setOnClickListener(this);
+        findViewById(R.id.btn_agreement).setOnClickListener(this);
+
+        mTvLog = (TextView) findViewById(R.id.tv_log);
+
+        registerBroadcast();
+    }
+
+    private void showLog(final String msg) {
+        mTvLog.post(new Runnable() {
+            @Override
+            public void run() {
+                mTvLog.setText(msg);
+            }
+        });
+    }
+
+    /**
+     * 获取token | get push token
+     */
+    private void getToken() {
+        showLog("get token: begin");
+        HMSAgent.Push.getToken(new GetTokenHandler() {
+            @Override
+            public void onResult(int rtnCode) {
+                showLog("get token: end code=" + rtnCode);
+            }
+        });
+    }
+
+    /**
+     * 删除token | delete push token
+     */
+    private void deleteToken() {
+        showLog("deleteToken:begin");
+        HMSAgent.Push.deleteToken(token, new DeleteTokenHandler() {
+            @Override
+            public void onResult(int rst) {
+                showLog("deleteToken:end code=" + rst);
+            }
+        });
+    }
+
+    /**
+     * 获取push状态 | Get Push State
+     */
+    private void getPushStatus() {
+        showLog("getPushState:begin");
+        HMSAgent.Push.getPushState(new GetPushStateHandler() {
+            @Override
+            public void onResult(int rst) {
+                showLog("getPushState:end code=" + rst);
+            }
+        });
+    }
+
+    /**
+     * 设置是否接收普通透传消息 | Set whether to receive normal pass messages
+     *
+     * @param enable 是否开启 | enabled or not
+     */
+    private void setReceiveNormalMsg(boolean enable) {
+        showLog("enableReceiveNormalMsg:begin");
+        HMSAgent.Push.enableReceiveNormalMsg(enable, new EnableReceiveNormalMsgHandler() {
+            @Override
+            public void onResult(int rst) {
+                showLog("enableReceiveNormalMsg:end code=" + rst);
+            }
+        });
+    }
+
+    /**
+     * 设置接收通知消息 | Set up receive notification messages
+     *
+     * @param enable 是否开启 | enabled or not
+     */
+    private void setReceiveNotifyMsg(boolean enable) {
+        showLog("enableReceiveNotifyMsg:begin");
+        HMSAgent.Push.enableReceiveNotifyMsg(enable, new EnableReceiveNotifyMsgHandler() {
+            @Override
+            public void onResult(int rst) {
+                showLog("enableReceiveNotifyMsg:end code=" + rst);
+            }
+        });
+    }
+
+    /**
+     * 显示push协议 | Show Push protocol
+     */
+    private void showAgreement() {
+        showLog("queryAgreement:begin");
+        HMSAgent.Push.queryAgreement(new QueryAgreementHandler() {
+            @Override
+            public void onResult(int rst) {
+                showLog("queryAgreement:end code=" + rst);
+            }
+        });
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_push) {
+            // 本页面切换到本页面的按钮事件不处理 | "This page switches to itself" button event does not need to be handled
+            return;
+        } else {
+            // 如果不是tab切换按钮则处理业务按钮事件 | Handle Business button events without the TAB toggle button
+            switch (id) {
+                case R.id.btn_gettoken:
+                    getToken();
+                    break;
+                case R.id.btn_deletetoken:
+                    deleteToken();
+                    break;
+                case R.id.btn_getpushstatus:
+                    getPushStatus();
+                    break;
+                case R.id.btn_setnormal:
+                    setReceiveNormalMsg(true);
+                    break;
+                case R.id.btn_setnofify:
+                    setReceiveNotifyMsg(true);
+                    break;
+                case R.id.btn_agreement:
+                    showAgreement();
+                    break;
+                default:
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PushMessageRevicer.unRegisterPushCallback(this);
+    }
+
+    /**
+     * 以下代码为sample自身逻辑，和业务能力不相关
+     * 作用仅仅为了在sample界面上显示push相关信息
+     */
+    private void registerBroadcast() {
+        PushMessageRevicer.registerPushCallback(this);
+    }
+
+    @Override
+    public void onReceive(Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            Bundle b = intent.getExtras();
+            if (b != null && PushMessageRevicer.ACTION_TOKEN.equals(action)) {
+                token = b.getString(PushMessageRevicer.ACTION_TOKEN);
+                showLog(token);
+            } else if (b != null && PushMessageRevicer.ACTION_UPDATEUI.equals(action)) {
+                String log = b.getString("log");
+                showLog(log);
+            }
+        }
+    }
+}
--- demo/src/main/java/com/netease/edu/study/message/PushMessageRevicer.java
+++ demo/src/main/java/com/netease/edu/study/message/PushMessageRevicer.java
@@ -1,42 +1,110 @@
 package com.netease.edu.study.message;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.huawei.hms.support.api.push.PushReceiver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hanpfei0306 on 18-7-17.
  */
 
 public class PushMessageRevicer extends PushReceiver {
-    @Override
-    public boolean onPushMsg(Context context, byte[] bytes, Bundle bundle) {
-        return super.onPushMsg(context, bytes, bundle);
+    public static final String ACTION_UPDATEUI = "action.updateUI";
+    public static final String ACTION_TOKEN = "action.updateToken";
+
+    private static List<IPushCallback> pushCallbacks = new ArrayList<IPushCallback>();
+    private static final Object CALLBACK_LOCK = new Object();
+
+    public static void registerPushCallback(IPushCallback callback) {
+        synchronized (CALLBACK_LOCK) {
+            pushCallbacks.add(callback);
+        }
+    }
+
+    public static void unRegisterPushCallback(IPushCallback callback) {
+        synchronized (CALLBACK_LOCK) {
+            pushCallbacks.remove(callback);
+        }
     }
 
     @Override
-    public void onPushMsg(Context context, byte[] bytes, String s) {
-        super.onPushMsg(context, bytes, s);
+    public void onToken(Context context, String tokenIn, Bundle extras) {
+        String belongId = extras.getString("belongId");
+
+        Intent intent = new Intent();
+        intent.setAction(ACTION_TOKEN);
+        intent.putExtra(ACTION_TOKEN, tokenIn);
+        callBack(intent);
+
+        intent = new Intent();
+        intent.setAction(ACTION_UPDATEUI);
+        intent.putExtra("log", "belongId is:" + belongId + " Token is:" + tokenIn);
+        callBack(intent);
     }
 
     @Override
-    public void onPushState(Context context, boolean b) {
-        super.onPushState(context, b);
+    public boolean onPushMsg(Context context, byte[] msg, Bundle bundle) {
+        try {
+            //CP可以自己解析消息内容，然后做相应的处理 | CP can parse message content on its own, and then do the appropriate processing
+            String content = new String(msg, "UTF-8");
+            Intent intent = new Intent();
+            intent.setAction(ACTION_UPDATEUI);
+            intent.putExtra("log", "Receive a push pass message with the message:" + content);
+            callBack(intent);
+        } catch (Exception e) {
+            Intent intent = new Intent();
+            intent.setAction(ACTION_UPDATEUI);
+            intent.putExtra("log", "Receive push pass message, exception:" + e.getMessage());
+            callBack(intent);
+        }
+        return false;
+    }
+
+    public void onEvent(Context context, Event event, Bundle extras) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_UPDATEUI);
+
+        int notifyId = 0;
+        if (Event.NOTIFICATION_OPENED.equals(event) || Event.NOTIFICATION_CLICK_BTN.equals(event)) {
+            notifyId = extras.getInt(BOUND_KEY.pushNotifyId, 0);
+            if (0 != notifyId) {
+                NotificationManager manager = (NotificationManager) context
+                        .getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.cancel(notifyId);
+            }
+        }
+
+        String message = extras.getString(BOUND_KEY.pushMsgKey);
+        intent.putExtra("log", "Received event,notifyId:" + notifyId + " msg:" + message);
+        callBack(intent);
+        super.onEvent(context, event, extras);
     }
 
     @Override
-    public void onToken(Context context, String s) {
-        super.onToken(context, s);
+    public void onPushState(Context context, boolean pushState) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_UPDATEUI);
+        intent.putExtra("log", "The Push connection status is:" + pushState);
+        callBack(intent);
     }
 
-    @Override
-    public void onToken(Context context, String s, Bundle bundle) {
-        super.onToken(context, s, bundle);
+    private static void callBack(Intent intent) {
+        synchronized (CALLBACK_LOCK) {
+            for (IPushCallback callback : pushCallbacks) {
+                if (callback != null) {
+                    callback.onReceive(intent);
+                }
+            }
+        }
     }
 
-    @Override
-    public void onEvent(Context context, Event event, Bundle bundle) {
-        super.onEvent(context, event, bundle);
+    public interface IPushCallback {
+        void onReceive(Intent intent);
     }
 }
